5ecd33d48fb39ac8b694b485a1f89a251f7df7a1
Tim-THINK\Tim <timothyshouldice@gmail.com>
2018-01-18 21:09:39 -0500

    Initial commit

---
+++ src/main/java/com/shouldice/recipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.shouldice.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        System.out.println("Testing 123");
+        return "index";
+    }
+}
